7e57f47daf2b0537fde9d28d159db935d824081a
AlejandroPadilla780 <alejandro.padilla.02029@maristascomayagua.edu.hn>
2025-08-21 21:09:45 -0600

    Update create_asistencia.sql

--- sql create/create_asistencia.sql
+++ sql create/create_asistencia.sql
@@ -1,8 +1,1 @@
-CREATE TABLE Asistencia_Clases (
-    id_asistencia INT PRIMARY KEY AUTO_INCREMENT,
-    id_miembro INT,
-    id_clase INT,
-    fecha DATE NOT NULL,
-    FOREIGN KEY (id_miembro) REFERENCES Miembros(id_miembro),
-    FOREIGN KEY (id_clase) REFERENCES Clases(id_clase)
-);
+
