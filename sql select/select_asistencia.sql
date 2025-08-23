2866bdbacb12e660b16e59ebcc70293085853aa3
Tifanny Cácerese <venturatifanny19@gmail.com>
2025-08-22 20:26:13 -0600

    Tifanny Cacére

---
+++ sql select/select_asistencia.sql
@@ -0,0 +1,6 @@
+select_06_asistencia.sql
+-- Registro de asistencia con nombres de miembros y clases
+SELECT m.nombre, m.apellido, c.nombre AS clase, a.fecha
+FROM Asistencia_Clases a
+JOIN Miembros m ON a.id_miembro = m.id_miembro
+JOIN Clases c ON a.id_clase = c.id_clase;
