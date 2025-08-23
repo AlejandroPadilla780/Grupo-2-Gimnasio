80f95b145d626f3553e4d5d0018e91b209ad0f9f
AlejandroPadilla780 <alejandro.padilla.02029@maristascomayagua.edu.hn>
2025-08-22 20:33:40 -0600

    Update select_entrenadores.sql

--- sql select/select_entrenadores.sql
+++ sql select/select_entrenadores.sql
@@ -1,5 +1,3 @@
-select_07_entrenadores.sql
--- Entrenadores y sus clases asignadas
 SELECT e.nombre, e.especialidad, c.nombre AS clase
 FROM Entrenadores e
 LEFT JOIN Clases c ON e.id_entrenador = c.id_entrenador;
